318c372ff7e2785582316b4b74747e6a451d5adf
Maksym Naletov <maksym.naletov@pricehubble.com>
2026-03-10 23:28:04 +0400

    added githaub pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://carbasix.com",
+  site: 'https://m0nk-tnd.github.io',
+  base: '/carbasix-landing',
   integrations: [tailwind()],
 });
